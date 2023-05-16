02a33a3bde070fa35580590b16798b74afd371a7
hadifayazi <hadifayazi4@gmail.com>
2023-05-17 00:14:16 +0200

    starter register form with mui

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homaPage/HomePage";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Register from "./pages/login-signup/Register";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
--- client/src/pages/login-signup/Register.jsx
+++ client/src/pages/login-signup/Register.jsx
@@ -1,128 +1,277 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import HowToRegIcon from "@mui/icons-material/HowToReg";
+// import { useState, useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
+// import HowToRegIcon from "@mui/icons-material/HowToReg";
+
+// function Register() {
+//   const onChange = (event) => {}
+//   const onSubmit = () => {};
+//   return (
+//     <>
+//       <section className="heading">
+//         <h1>
+//           <HowToRegIcon /> Register
+//         </h1>
+//         <p>Please create an account</p>
+//       </section>
+
+//       <section className="form">
+//         <form onSubmit={onSubmit}>
+//           <div className="form-group">
+//             <input
+//               type="text"
+//               className="form-control"
+//               id="name"
+//               name="name"
+//               value={name}
+//               placeholder="Enter your name"
+//               onChange={onChange}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <input
+//               type="email"
+//               className="form-control"
+//               id="email"
+//               name="email"
+//               value={email}
+//               placeholder="Enter your email"
+//               onChange={onChange}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <input
+//               type="password"
+//               className="form-control"
+//               id="password"
+//               name="password"
+//               value={password}
+//               placeholder="Enter password"
+//               onChange={onChange}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <input
+//               type="password"
+//               className="form-control"
+//               id="password2"
+//               name="password2"
+//               value={password2}
+//               placeholder="Confirm password"
+//               onChange={onChange}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <button type="submit" className="btn btn-block">
+//               Submit
+//             </button>
+//           </div>
+//         </form>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Register;
+
+import {
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { Formik } from "formik";
+import * as yup from "yup";
+import Dropzone from "react-dropzone";
+import FlexBetween from "../../components/FlexBetween";
+
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+  passwordConfirmation: yup.string().required(),
+  location: yup.string().required(),
+  picture: yup.string().required(),
+});
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
 
 function Register() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
+  const { palette } = useTheme();
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-  const { name, email, password, password2 } = formData;
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  );
-
-  useEffect(() => {
-    if (isError) {
-      toast.error(message);
+  const registerForm = async (values, onSubmitProps) => {
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
     }
-
-    if (isSuccess || user) {
-      navigate("/");
-    }
-
-    dispatch(reset());
-  }, [user, isError, isSuccess, message, navigate, dispatch]);
-
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
+    formData.append("picturePath", values.picture.name);
   };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    if (password !== password2) {
-      toast.error("Passwords do not match");
-    } else {
-      const userData = {
-        name,
-        email,
-        password,
-      };
-
-      dispatch(register(userData));
-    }
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    registerForm(values, onSubmitProps);
   };
-
-  if (isLoading) {
-    return <Spinner />;
-  }
 
   return (
-    <>
-      <section className="heading">
-        <h1>
-          <HowToRegIcon /> Register
-        </h1>
-        <p>Please create an account</p>
-      </section>
-
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              value={name}
-              placeholder="Enter your name"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              placeholder="Enter your email"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              placeholder="Enter password"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password2"
-              name="password2"
-              value={password2}
-              placeholder="Confirm password"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-block">
-              Submit
-            </button>
-          </div>
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={initialValuesRegister}
+      validationSchema={registerSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            sx={{
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+            }}
+          >
+            <>
+              <TextField
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName || ""}
+                name="firstName"
+                error={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                helperText={touched.firstName && errors.firstName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName || ""}
+                name="lastName"
+                error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={Boolean(touched.email) && Boolean(errors.email)}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                label="Password"
+                type="password"
+                autoComplete="on"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+                name="password"
+                error={Boolean(touched.password) && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                label="PasswordConfirmation"
+                type="password"
+                autoComplete="on"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+                name="passwordConfirmation"
+                error={
+                  Boolean(touched.passwordConfirmation) &&
+                  Boolean(errors.passwordConfirmation)
+                }
+                helperText={
+                  touched.passwordConfirmation && errors.passwordConfirmation
+                }
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                label="Location"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.location || ""}
+                name="location"
+                error={Boolean(touched.location) && Boolean(errors.location)}
+                helperText={touched.location && errors.location}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <Box
+                gridColumn="span 4"
+                border={`1px solid `}
+                borderRadius="5px"
+                p="1rem"
+              >
+                <Dropzone
+                  acceptedFiles=".jpg,.jpeg,.png"
+                  multiple={false}
+                  onDrop={(acceptedFiles) =>
+                    setFieldValue("picture", acceptedFiles[0])
+                  }
+                >
+                  {({ getRootProps, getInputProps }) => (
+                    <Box
+                      {...getRootProps()}
+                      border={`2px dashed ${palette.primary.main}`}
+                      p="1rem"
+                      sx={{ "&:hover": { cursor: "pointer" } }}
+                    >
+                      <input {...getInputProps()} />
+                      {!values.picture ? (
+                        <p>Add Picture Here</p>
+                      ) : (
+                        <FlexBetween>
+                          <Typography>{values.picture.name}</Typography>
+                          <EditOutlinedIcon />
+                        </FlexBetween>
+                      )}
+                    </Box>
+                  )}
+                </Dropzone>
+              </Box>
+            </>
+          </Box>
+          <Box>
+            <Button
+              fullWidth
+              type="submit"
+              sx={{
+                m: "2rem 0",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              REGISTER
+            </Button>
+          </Box>
         </form>
-      </section>
-    </>
+      )}
+    </Formik>
   );
 }
 
