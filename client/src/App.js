b37b7fc150dc794420ff68760ebfd7546474beee
hadifayazi <hadifayazi4@gmail.com>
2023-05-17 02:33:26 +0200

    adding theme provider to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login-signup/LoginPage";
 import HomePage from "./pages/homaPage/HomePage";
 import ProfilePage from "./pages/profilePage/ProfilePage";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Register from "./pages/login-signup/Register";
+import { themeSettings } from "./theme";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
-      <ToastContainer />
     </div>
   );
 }
