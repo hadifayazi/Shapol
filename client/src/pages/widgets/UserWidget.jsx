98f8036085652bbd4f14419949bcfdfd19e18488
hadifayazi <hadifayazi4@gmail.com>
2023-05-18 22:35:47 +0200

    user widget starter

---
+++ client/src/pages/widgets/UserWidget.jsx
@@ -0,0 +1,14 @@
+import {
+  ManageAccountsIcon,
+  EditIcon,
+  LocationOnIcon,
+  CalendarMonthIcon,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import { ProfilePicture } from "../../components/ProfilePicture";
+import { widgetWrapper } from "../../components/WidgetWrapper";
+
+export const UserWidget = () => {
+  return <div>UserWidget</div>;
+};
