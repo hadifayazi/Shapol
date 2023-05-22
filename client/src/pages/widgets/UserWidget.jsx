f04ebf598aecc88a4538d5d302041c2a1d7e3544
hadifayazi <hadifayazi4@gmail.com>
2023-05-22 12:17:44 +0200

    creating user widget

--- client/src/pages/widgets/UserWidget.jsx
+++ client/src/pages/widgets/UserWidget.jsx
@@ -1,20 +1,89 @@
 import {
-  ManageAccountsIcon,
-  EditIcon,
-  LocationOnIcon,
-  CalendarMonthIcon,
+  Description,
+  ManageAccountsOutlined,
+  LocationOnOutlined,
 } from "@mui/icons-material";
 import { Box, Typography, Divider, useTheme } from "@mui/material";
-import FlexBetween from "../../components/FlexBetween";
 import { ProfilePicture } from "../../components/ProfilePicture";
-import { widgetWrapper } from "../../components/WidgetWrapper";
+import FlexBetween from "../../components/FlexBetween";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useNavigate } from "react-router-dom";
 import { useGetMeQuery } from "../../store/api/authApi";
-import { useSelector } from "react-redux";
 
 export const UserWidget = () => {
-  //   const user = useGetMeQuery(null, { selectFromResult: ({ data }) => data });
-  //   console.log(user);
-  const user = useGetMeQuery();
-  console.log(user);
-  return <Box>user widget</Box>;
+  const { data } = useGetMeQuery();
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
+
+  const {
+    _id,
+    firstName,
+    lastName,
+    location,
+    bio,
+    createdAt,
+    friends,
+    picturePath,
+  } = data;
+
+  return (
+    <WidgetWrapper>
+      {/* PROFILEPIC + NAME + FRIENDS*/}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${_id}`)}
+      >
+        <FlexBetween gap="1rem">
+          <ProfilePicture image={picturePath} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length} friends</Typography>
+          </Box>
+        </FlexBetween>
+        <ManageAccountsOutlined />
+      </FlexBetween>
+
+      <Divider />
+
+      {/* BIO + LOCATION */}
+      <Box p="1rem 0">
+        <Box display="flex" alignItems="center" gap="1rem">
+          <Description fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{bio}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{location}</Typography>
+        </Box>
+      </Box>
+
+      <Divider />
+
+      {/* JOIN /createdat */}
+      <Box p="1rem 0">
+        <FlexBetween mb="0.5rem">
+          <Typography color={medium}>Joined:</Typography>
+          <Typography color={main} fontWeight="500">
+            {createdAt}
+          </Typography>
+        </FlexBetween>
+      </Box>
+    </WidgetWrapper>
+  );
 };
