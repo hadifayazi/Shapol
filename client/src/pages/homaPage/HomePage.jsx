1698a107b5300b356667d56e35e2fec809615505
hadifayazi <hadifayazi4@gmail.com>
2023-05-25 03:07:27 +0200

    home Page sekelton

--- client/src/pages/homaPage/HomePage.jsx
+++ client/src/pages/homaPage/HomePage.jsx
@@ -1,14 +1,36 @@
 import NavbarPage from "../navbar/NavbarPage";
 import { UserWidget } from "../widgets/UserWidget";
-import IsAuth from "../../components/IsAuth";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+// import MyPostWidget from "scenes/widgets/MyPostWidget";
+// import PostsWidget from "scenes/widgets/PostsWidget";
+// import AdvertWidget from "scenes/widgets/AdvertWidget";
+// import FriendListWidget from "scenes/widgets/FriendListWidget";
 
-export const HomePage = () => {
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.auth.user);
+
   return (
-    <div>
+    <Box>
       <NavbarPage />
-      <UserWidget />
-      <IsAuth />
-    </div>
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
+    </Box>
   );
 };
 
